6f01eb014ac6bd2bf0a360cf95bb7bff68cd5663
dimact22 <90906931+dimact22@users.noreply.github.com>
2021-09-19 22:34:58 +0300

    ticketinmylife

--- main.js
+++ main.js
@@ -1,96 +1,89 @@
-'use strict'
+var name = 'Петья';
+var surname = 'Васичкин';
+var age = 25;
 
-const USD = 27.2635;
-const EUR = 32.0591;
-const UAH = 1;
-const RUB = 0.3660;
+var c = false;
+var d = 5;
 
-let input = document.getElementById('input');
-let output = document.getElementById('output');
-let optionInput = document.getElementById('optionInput');
-let optionOutput = document.getElementById('optionOutput');
+// a = 456;
 
-let currency = EUR;
-let convertedCurrency = UAH;
+// a = prompt ('Напишите значение переменной a');
 
-input.addEventListener('input', convert);
-optionInput.addEventListener('change', selectOptionInput);
-optionOutput.addEventListener('change', selectOptionOutput);
+// if (a == 37) {
+// 	alert ('a = 37');
+// } else {
+// 	alert ('что-то другое')
+// }
 
-function selectOptionInput() {
-    calculateCurrency();
-    convert();
-}
-function selectOptionOutput() {
-    calculateCurrencyConvert();
-    convert();
-}
+// for (var i = 0;i < 5;i++) {
+// 	alert (i);
+// }
 
-function calculateCurrency() {
-    currency = (optionInput.value == 'USD') ? USD :
-        (optionInput.value == 'EUR') ? EUR :
-            (optionInput.value == 'UAH') ? UAH
-                : RUB;
+
+var user = {
+	'name': 'Петья',
+	'surname': 'Васичкин',
+	'age': 25,
+	'getElementById' : function () {}
 }
 
-function calculateCurrencyConvert() {
-    convertedCurrency = (optionOutput.value == 'USD') ? USD :
-        (optionOutput.value == 'EUR') ? EUR :
-            (optionOutput.value == 'UAH') ? UAH
-                : RUB;
+// alert ( document.getElementById );
+
+var mas = ['Петья', 'Васичкин', 25];
+
+// alert (mas[2]);
+
+
+
+function doMagic (a,dfdf) {
+	alert (a + dfdf);
 }
 
-function convert() {
-    output.value = (input.value * currency / convertedCurrency).toFixed(2);
+// doMagic(10,5);
+// doMagic(1,1);
+
+
+
+var btn = document.getElementById('btn');
+
+btn.onclick = function (e) {
+	e.preventDefault();
+	var text = document.querySelector('p.intro');
+	text.classList.add('red');
+	var img = document.querySelector('.desktop');
+	img.style.display = 'none';
+	document.querySelector('.skill-change').style.marginLeft = '50px';
 }
- function countCurrency() {
-     let result = 0;
-     if (optionsInput.value == 'eur') {
-         if (optionsOutput.value == 'uah') {
-             result = enterInput.value * 32.3834;
-             resultInput.value = result;
-         } else if (optionsOutput.value == 'usd') {
-             result = enterInput.value * 1.1872;
-             resultInput.value = result;
-         } else if (optionsOutput.value == 'rub') {
-             result = enterInput.value * 88.6050;
-             resultInput.value = result;
-         }
-     }
-     if (optionsInput.value == 'uah') {
-         if (optionsOutput.value == 'eur') {
-             result = enterInput.value * 0.0309;
-             resultInput.value = result;
-         } else if (optionsOutput.value == 'usd') {
-             result = enterInput.value * 0.0367;
-             resultInput.value = result;
-         } else if (optionsOutput.value == 'rub') {
-            result = enterInput.value * 2.7361;
-             resultInput.value = result;
-         }
-     }
-     if (optionsInput.value == 'usd') {
-         if (optionsOutput.value == 'eur') {
-             result = enterInput.value * 0.8423;
-            resultInput.value = result;
-         } else if (optionsOutput.value == 'uah') {
-             result = enterInput.value * 27.2772;
-            resultInput.value = result;
-        } else if (optionsOutput.value == 'rub') {
-          result = enterInput.value * 74.6336;            
- resultInput.value = result;
-         }
-     }
-     if (optionsInput.value == 'rub') {
-         if (optionsOutput.value == 'eur') {
-             result = enterInput.value * 0.0113;
-             resultInput.value = result;
-         } else if (optionsOutput.value == 'usd') {
-             result = enterInput.value * 0.0134;
-             resultInput.value = result;
-         } else if (optionsOutput.value == 'uah') {
-            result = enterInput.value * 0.3655;
-             resultInput.value = result;
-         }
-     }
- }
+
+$(function () {
+	$(window).scroll(function() {
+	    $('#future .section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.trigger').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.container').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInRight");
+	        }
+	    });
+	});
+})
